ce8f418c1b901d9add791e4cc23d8174d13fb87d
root <root@LAPTOP-D3FG1VK4.localdomain>
2026-07-25 19:27:35 +0100

    £$£

--- frontend/src/contexts/useAuth.ts
+++ frontend/src/contexts/useAuth.ts
@@ -1,5 +1,5 @@
 export function useAuth() {
-  const context = useContext(AuthProvider)
+  const context = useContext(AuthContext)
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider')
   }
