ad99a526c6a4d59821466331b454584429c07512
root <root@LAPTOP-D3FG1VK4.localdomain>
2026-07-26 18:08:54 +0100

    4

--- frontend/src/components/ConsumptionChart.tsx
+++ frontend/src/components/ConsumptionChart.tsx
@@ -124,7 +124,7 @@ export default function ConsumptionChart({
       
       interface TooltipParam {
         dataIndex: number
-        },
+      }
 
         formatter: (params: ConsumptionChartProps) => {
           const param = params[0]
