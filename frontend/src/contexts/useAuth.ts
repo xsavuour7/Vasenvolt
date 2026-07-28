6ebb06259a41ee23ea31d1515df4122a8bab5006
root <root@LAPTOP-D3FG1VK4.localdomain>
2026-07-28 21:13:15 +0100

    9

--- frontend/src/contexts/useAuth.ts
+++ frontend/src/contexts/useAuth.ts
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { AuthProvider } from './AuthContext'
 export function useAuth(): {
- const context = useContext(AuthProvider)
+const context = useContext(AuthProvider)
  if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider')
   }
