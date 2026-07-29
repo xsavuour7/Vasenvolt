7f05f9f7f62e2f5f8b35e9f3b823c0127c9fea01
root <root@LAPTOP-D3FG1VK4.localdomain>
2026-07-29 19:25:37 +0100

    0

--- frontend/src/contexts/useAuth.ts
+++ frontend/src/contexts/useAuth.ts
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { AuthContext } from './AuthContextDef'
 import type { AuthContextType } from '.AuthContextDef'
+
 export function useAuth(): AuthContextType {
 const context = useContext(AuthContext)
  if (context === undefined) {
